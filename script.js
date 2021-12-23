31de889a8e90245a35e29e1e88a00b11774445d4
PedroVader <96238562+PedroVader@users.noreply.github.com>
2021-12-23 15:16:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function generate ()  {
     ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
     var newColor = "#";
 
+
+    //Para cambiar la longitud del valor hexadecimal cambia la condición númerica del bucle for
     for(var i = 0; i < 8; i++) {
         var x = Math.round(Math.random() * 14);
         var y = valoresHexadecimales[x];
