458fc72d76c5017b192014998f2d4669feb6e6fd
PedroVader <96238562+PedroVader@users.noreply.github.com>
2021-12-23 15:13:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function generate ()  {
+    var valoresHexadecimales = 
+    ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+    var newColor = "#";
+
+    for(var i = 0; i < 8; i++) {
+        var x = Math.round(Math.random() * 14);
+        var y = valoresHexadecimales[x];
+        newColor += y; 
+    }
+
+    document.getElementById("container").style.backgroundColor = newColor;
+
+    document.getElementById("output").innerHTML = newColor;
+}
+
+generate();
